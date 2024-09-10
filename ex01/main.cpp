a9a6a672288edd6dc04bf5925502f2b927718c6e
Boris Randebrock <brandebr@student.42barcelona.com>
2024-09-10 11:12:31 +0200

    munching some more

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brandebr <brandebr@42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 13:51:15 by brandebr          #+#    #+#             */
-/*   Updated: 2024/09/10 10:34:41 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/09/10 11:11:04 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include "Zombie.hpp"
+
+void	help() {
+	sleep(1);
+	std::cout << std::endl << "                                    HELP, 😱... Zombies  🧟 🧟🧟🧟.... 😨" << std::endl;
+	sleep(2);
+	std::cout << std::endl << "Dont you worry, van Helsing to rescue..🏃" << std::endl;
+	sleep(3);
+}
 
 int	main(int argc, char **argv) {
     std::cout << "                         _____" << std::endl;
@@ -44,19 +52,23 @@ int	main(int argc, char **argv) {
 
     std::cout << RESET;
     std::cout << std::endl;
-    if (argc > 1)
+    if (argc > 1 && argc < 4)
     {
-	int num = atoi(argv[1]);
-	Zombie *horde = zombieHorde(num, argv[2]);
+		int num = atoi(argv[1]);
+		Zombie *horde = zombieHorde(num, argv[2]);
     	for (int i = 0; i < num; i++) {
 		horde[i].announce();
 	}
-	sleep(1);
-	std::cout << "HELP, 😱... Zombies  🧟 🧟🧟🧟.... 😨" << std::endl;
-	sleep(2);
-	std::cout << "Dont you worry, van Helsing to rescue..🏃" << std::endl;
-	sleep(3);
+	help();
     delete[] horde;
-    }
+    } else {
+		Zombie *horde = zombieHorde(13, "Boo");
+		for (int i = 0; i < 13; i++) {
+			horde[i].announce();
+		}
+		help();
+
+    delete[] horde;
+	}
     return 0;
 }
