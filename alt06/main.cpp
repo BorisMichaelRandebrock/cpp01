825dbfb8c283ebcd7ccf5a015b6607e537729fcc
Boris Randebrock <brandebr@student.42barcelona.com>
2024-09-16 11:50:34 +0200

    forgot to delete...

--- alt06/main.cpp
+++ alt06/main.cpp
@@ -6,29 +6,12 @@
 /*   By: brandebr <brandebr@42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 18:24:00 by brandebr          #+#    #+#             */
-/*   Updated: 2024/09/16 11:44:38 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/09/16 11:48:40 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "Harl.hpp"
-
-enum	Options {
-	DEBUG,
-	INFO,
-	WARNING,
-	ERROR,
-	WARN,
-};
-
-Options stringEnum(const std::string &input) {
-	if (input == "DEBUG") return DEBUG;
-	else if (input == "INFO") return INFO;
-	else if (input == "WARNING") return WARNING;
-	else if (input == "ERROR") return ERROR;
-	else return WARN;
-}
-
 
 int main(int argc, char **argv) {
 	Harl harl;
@@ -38,6 +21,5 @@ int main(int argc, char **argv) {
 	} else { 
 		harl.complain("Whatever is wrong");
 	}
-
 	return 0;
 }
